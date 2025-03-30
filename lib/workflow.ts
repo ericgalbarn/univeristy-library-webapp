8c3a4ffd55496f8b9e9488b74ce2af739f2b8c91
ericgalbarn <ericgalbarn.com>
2025-03-30 14:09:17 +0700

    Write workflows for email sending functionality for the project

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,35 @@
 import { Client as WorkflowClient } from "@upstash/workflow";
 import config from "./config";
+import { Client as QstashClient, resend } from "@upstash/qstash";
 
 export const workflowClient = new WorkflowClient({
   baseUrl: config.env.upstash.qstashUrl,
   token: config.env.upstash.qstashToken,
 });
+
+const qstashClient = new QstashClient({
+  token: config.env.upstash.qstashToken,
+});
+
+export const sendEmail = async ({
+  email,
+  subject,
+  message,
+}: {
+  email: string;
+  subject: string;
+  message: string;
+}) => {
+  await qstashClient.publishJSON({
+    api: {
+      name: "email",
+      provider: resend({ token: config.env.resendToken }),
+    },
+    body: {
+      from: "Eric Galbarn <hello.ericgalbarn.site>",
+      to: [email],
+      subject,
+      html: message,
+    },
+  });
+};
